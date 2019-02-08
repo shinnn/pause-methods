2cb812a5aa8064962643c20e93c0f50d430cd42d
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2019-02-08 18:49:09 +0900

    init

---
+++ README.md
@@ -0,0 +1,92 @@
+# pause-methods
+
+[![npm version](https://img.shields.io/npm/v/pause-methods.svg)](https://www.npmjs.com/package/pause-methods)
+[![Build Status](https://travis-ci.com/shinnn/pause-methods.svg?branch=master)](https://travis-ci.com/shinnn/pause-methods)
+[![codecov](https://codecov.io/gh/shinnn/pause-methods/branch/master/graph/badge.svg)](https://codecov.io/gh/shinnn/pause-methods)
+
+A [Node.js](https://nodejs.org/) module to pause/resume execution of the object's methods
+
+```javascript
+const {pause, resume} = require('pause-methods');
+
+const obj = {
+  methodA() {
+    process.stdout.write('hello');
+  },
+  methodB() {
+    process.stdout.write('world');
+  }
+};
+const pausedObj = pause(obj);
+
+pausedObj.methodA(); // prints nothing
+pausedObj.methodB(); // prints nothing
+
+resume(pausedObj); // prints 'hello' and 'world'
+```
+
+## Installation
+
+[Use](https://docs.npmjs.com/cli/install) [npm](https://docs.npmjs.com/about-npm/).
+
+```
+npm install pause-methods
+```
+
+## API
+
+```javascript
+const pauseMethods = require('pause-methods');
+```
+
+### pauseMethods(*obj*)
+
+*obj*: `Object`  
+Return: `Object` (a reference to *obj*)
+
+It makes *obj*'s [enumerable](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Enumerability_and_ownership_of_properties) `Function` properties do nothing and return `undefined`.
+
+```javascript
+let num = 0;
+const obj = {countUp: () => ++num};
+
+obj.countUp(); //=> 1
+num; //=> 1
+
+const pausedObj = pauseFn(obj);
+
+pausedObj.countUp(); //=> undefined, not 2
+num; //=> 1, not 2
+
+pausedObj.countUp(); //=> undefined
+pausedObj.countUp(); //=> undefined
+pausedObj.countUp(); //=> undefined
+// ...
+num; //=> 1
+```
+
+### pauseMethods.pause(*obj*)
+
+An alias of [`pauseMethods()`](#pausemethodsobj).
+
+### pauseMethods.resume(*obj*)
+
+*obj*: `Object` returned by [`pauseMethods()`](#pausemethodsobj)  
+Return: `Object` (a reference to *obj*)
+
+It restores the original functionality of the paused methods, calls it with each arguments passed while paused.
+
+```javascript
+const pausedFs = pauseMethods(fs.promises);
+
+(async () => {
+  await pausedFs.writeFile('./a', 'foo'); // ./a is not created here
+  await pausedFs.mkdir('./b'); // ./b is not created here
+
+  pauseFs.resume(); //=> ./a and ./b are created here
+})();
+```
+
+## License
+
+[ISC License](./LICENSE) © 2019 Shinnosuke Watanabe
---
+++ index.js
@@ -0,0 +1,86 @@
+'use strict';
+
+var inspect = require('util').inspect;
+
+var appendType = require('append-type');
+var Map = require('es6-map');
+var pauseFn = require('pause-fn');
+var WeakMap = require('es6-weak-map');
+
+var PAUSE_ERROR = 'Expected an <Object> whose methods are paused by `pauseMethods()`';
+var pausedObjMethodsMap = new WeakMap();
+var resumed = new Set();
+
+module.exports = function pauseMethods(obj) {
+	if (obj === null || typeof obj !== 'object') {
+		var error0 = new TypeError('Expected an <Object>, but got a non-object value ' + appendType(obj) + '.');
+		error0.code = 'ERR_INVALID_ARG_TYPE';
+
+		throw error0;
+	}
+
+	if (pausedObjMethodsMap.has(obj)) {
+		var error1 = new Error('Expected an <Object> which hasn\'t been paused by `pauseMethods()` yet, but got an already paused one ' + inspect(obj, {breakLength: Infinity}) + '.');
+		error1.code = 'ERR_INVALID_ARG_ALUE';
+
+		throw error1;
+	}
+
+	var methods = new Map();
+
+	Object.keys(obj).forEach(function(key) {
+		var propertyDescriptor = Object.getOwnPropertyDescriptor(obj, key);
+		var value = propertyDescriptor.value;
+		var writable = propertyDescriptor.writable;
+
+		if (typeof value !== 'function' || !writable) {
+			return;
+		}
+
+		methods.set(key, value);
+		obj[key] = pauseFn(value.bind(obj));
+	});
+	pausedObjMethodsMap.set(obj, methods);
+	resumed.delete(obj);
+
+	return obj;
+};
+
+Object.defineProperty(module.exports, 'pause', {
+	enumerable: true,
+	value: module.exports
+});
+
+Object.defineProperty(module.exports, 'resume', {
+	enumerable: true,
+	value: function resume(obj) {
+		if (obj === null || typeof obj !== 'object') {
+			var error = new TypeError('Expected an <Object>, but got a non-object value ' + appendType(obj) + '.');
+			error.code = 'ERR_INVALID_ARG_TYPE';
+
+			throw error;
+		}
+
+		if (resumed.has(obj)) {
+			var error1 = new Error(PAUSE_ERROR + ', but got an already resume()-ed one ' + inspect(obj, {breakLength: Infinity}) + '.');
+			error1.code = 'ERR_INVALID_ARG_VALUE';
+
+			throw error1;
+		}
+
+		if (!pausedObjMethodsMap.has(obj)) {
+			var error2 = new Error(PAUSE_ERROR + ', but got ' + inspect(obj, {breakLength: Infinity}) + ' which is not paused by `pauseMethods()`.');
+			error2.code = 'ERR_INVALID_ARG_VALUE';
+
+			throw error2;
+		}
+
+		pausedObjMethodsMap.get(obj).forEach(function(value, key) {
+			pauseFn.resume(obj[key]);
+			obj[key] = value;
+		});
+		resumed.add(obj);
+
+		return obj;
+	}
+});
